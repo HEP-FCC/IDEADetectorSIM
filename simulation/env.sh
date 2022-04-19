765a1be85df14af4bc348df9b484c92fd556b0b3
Iacopo Vivarelli <iacopo.vivarelli@cern.ch>
2022-04-19 15:40:50 +0100

    CMakeLists.txt

--- simulation/env.sh
+++ simulation/env.sh
@@ -1,17 +1,10 @@
 #!/bin/bash
 
-#
-echo "sourcing the latext key4hep stack"
-source /cvmfs/sw.hsf.org/key4hep/setup.sh
-#
-export PRJBASE="/afs/cern.ch/work/w/welmeten/public/IDEA_calo_Key4HEP/DriftChamberPLUSVertex"
-echo "PRJBASE set $PRJBASE"
-
 # suggestions by Valentin to retrieve necessary env for the rest of compilation 
 for path in ${CMAKE_PREFIX_PATH//:/ }; do
 
     # GEANT4 --------------------------------------
-    if  grep  -q '/geant4/[0-9]' <<< "$path"  ; then
+    if  grep  -q '/geant4-10.7.1' <<< "$path"  ; then
         echo "$path"
 	export G4BASE=$path
 	export G4INSTALL=$G4BASE/share/Geant4-10.7.1/geant4make
@@ -32,7 +25,4 @@ done
 echo "GEANT4 is set to $G4BASE"
 echo "CLHEP is set to $CLHEP_BASE_DIR"
 
-# simulation (see README.md)
-export SIM_INSTAL_DIR=$PRJBASE/simulation/instal_dir # <installation_directory>
-export SIM_OUTPUT_DIR=$PRJBASE/simulation/output_dir # <output_directory>
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$SIM_INSTAL_DIR/lib
+
